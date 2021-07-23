b9a9e37a0d8f948873ed7fa81cbc538ae2ccbf0b
Krish <krpxbox@gmail.com>
2021-07-23 18:01:04 +0000

    starting add workout

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
-
+console.log("script running")
 const addWorkout = 
 () => {
     const userWorkout = document.querySelector("#user-in").value;
 
-    console.log(passcodeIn);
+    console.log(userWorkout);
 
     const payload = {
         workout: userWorkout,
     };
-    firebase.database().ref().workout.gym.push(payload);
+    const ref = firebase.database().ref().workouts;
+    console.log(ref)
 
 }
